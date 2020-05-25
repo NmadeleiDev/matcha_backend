fa51912cc8e12d7b34328a02fdfee6a3916eed9c
Григорий Потёмкин <potemkin3940@gmail.com>
2020-05-25 19:29:40 +0300

    hey

--- main_backend/db/postgres/utils.go
+++ main_backend/db/postgres/utils.go
@@ -1,2 +1,12 @@
 package postgres
 
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+func CalculateSha256(values string) string {
+	sha256Calculate := sha256.Sum256([]byte(values))
+	hash := fmt.Sprintf("%x", sha256Calculate)
+	return hash
+}
