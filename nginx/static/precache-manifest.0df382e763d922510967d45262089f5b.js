c8073932d77ebda5552705971e1da697425bfb58
Gregory Potemkin <gregory@gomobile.ru>
2020-12-05 20:11:05 +0300

    front

---
+++ nginx/static/precache-manifest.0df382e763d922510967d45262089f5b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16153bb02ce191d1b65eb6df4cdc872b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fee04f5acd535b4cdb1",
+    "url": "/static/css/main.2e481624.chunk.css"
+  },
+  {
+    "revision": "94537f08ebe91ffcb32b",
+    "url": "/static/js/2.2c583f67.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/static/js/2.2c583f67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fee04f5acd535b4cdb1",
+    "url": "/static/js/main.193411be.chunk.js"
+  },
+  {
+    "revision": "30230bc049e364f4fdd8",
+    "url": "/static/js/runtime-main.67eec890.js"
+  },
+  {
+    "revision": "0738679df4cf4e566f60343830da7bfa",
+    "url": "/static/media/Quicksand-Bold.0738679d.ttf"
+  },
+  {
+    "revision": "188ca708e4cbbbe3837c3d904b8eeb61",
+    "url": "/static/media/Quicksand-Light.188ca708.ttf"
+  },
+  {
+    "revision": "216d43ee8707910af457af569eda1dec",
+    "url": "/static/media/Quicksand-Regular.216d43ee.ttf"
+  },
+  {
+    "revision": "77fa00996ecb4104c7880b8749c7c4e0",
+    "url": "/static/media/Righteous-Regular.77fa0099.ttf"
+  },
+  {
+    "revision": "d2c893f55930c7cb5bfe41538be295d7",
+    "url": "/static/media/default-avatar.d2c893f5.png"
+  }
+]);
