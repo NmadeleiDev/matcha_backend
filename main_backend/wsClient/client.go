bdbee12f59b499da0aa058fd70aa55c7ca15ee15
Gregory Potemkin <gregory@gomobile.ru>
2020-12-05 19:57:44 +0300

    ws fix

--- main_backend/wsClient/client.go
+++ main_backend/wsClient/client.go
@@ -12,13 +12,13 @@ import (
 
 const (
 	// Time allowed to write a message to the peer.
-	writeWait = 10 * time.Second
+	writeWait = 3 * time.Second
 
 	// Time allowed to read the next pong message from the peer.
 	pongWait = 30 * time.Second
 
 	// Send pings to peer with this period. Must be less than pongWait.
-	pingPeriod = pongWait * 9 / 10
+	pingPeriod = pongWait * 8 / 10
 
 	// Maximum message size allowed from peer.
 	maxMessageSize = 1024
