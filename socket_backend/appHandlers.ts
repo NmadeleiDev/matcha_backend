e011c1aa2e63e002507e1348f96c3a3182d1dedd
Gregory Potemkin <gregory@gomobile.ru>
2020-12-27 14:36:04 +0300

    fix

--- socket_backend/appHandlers.ts
+++ socket_backend/appHandlers.ts
@@ -9,6 +9,7 @@ import * as queryString from "querystring";
 import {MongoManager} from './db/mongo/mongo'
 
 export function setOnlineState(id: string, state: boolean) {
+    console.log(`Setting online=${state} for ${id}`)
     MongoManager.setUserOnlineState(id, state)
         .then(() => console.log(`Set online=${state} for user ${id}`))
         .catch((e) => console.warn(`Error setting online=${state} for user ${id}! Error: ${e.toString()}`))
