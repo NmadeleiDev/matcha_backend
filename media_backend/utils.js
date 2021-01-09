ba3941491ece8f3f59ef9a2b7017e65684f612b8
Gregory Potemkin <gregory@gomobile.ru>
2021-01-09 13:25:08 +0300

    front

--- media_backend/utils.js
+++ media_backend/utils.js
@@ -4,14 +4,20 @@ const fs = require('fs');
 async function deleteImagesFromStorage(filenames) {
     console.log("Deleting files: ", filenames)
 
-    for (const filename in filenames) {
+    if (!Array.isArray(filenames)) {
+        return null
+    }
+
+    for (let i = 0; i < filenames.length; i++) {
+        const filename = filenames[i]
         if (typeof filename !== 'string')
             continue
-        try {
-            fs.unlinkSync(filename)
-        } catch (e) {
-            console.log("Error deleting files: ", e)
-        }
+        fs.unlink(filename, function(err){
+            if(err) {
+                console.log("Error deleting files: ", err)
+            }
+            console.log('Deleted! ', filenames);
+        });
     }
 }
 
