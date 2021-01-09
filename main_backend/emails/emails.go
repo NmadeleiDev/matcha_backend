e4d3fd1ea9c91e6c3a287a212b720a4deff50085
Gregory Potemkin <gregory@gomobile.ru>
2021-01-09 11:28:58 +0300

    front

--- main_backend/emails/emails.go
+++ main_backend/emails/emails.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/smtp"
 	"os"
+	"strings"
 
 	"backend/dao"
 
@@ -15,27 +16,44 @@ type EmailManager struct {
 
 var Manager dao.EmailService = &EmailManager{}
 
+var host = os.Getenv("PROJECT_HOST")
+
 func (m *EmailManager) SendPasswordResetEmail(to, key string) {
+	var link string
+	if strings.Contains(host, "localhost") {
+		link = "http://localhost:" + os.Getenv("PROJECT_PORT") + "/reset?k=" + key
+	} else {
+		link = "https://aim-love.ga/reset?k=" + key
+	}
+
 	template := `
 <div>
     <h2>Password reset</h2>
     <p>If you didn't request password reset for your Matcha account, just ignore this email.</p>
-    <p><a href="http://localhost:8080/reset?k=%s">Press here to reset password</a></p>
+    <p><a href="%v">Press here to reset password</a></p>
 </div>`
-	body := fmt.Sprintf(template, key)
+	body := fmt.Sprintf(template, link)
 	m.sendEmailFromService(to, "Matcha password reset", body)
 }
 
 func (m *EmailManager) SendAccountVerificationKey(to, key string) {
-	//link := "https://aim-love.ga/api/v1/verify/" + key
-	link := "http://localhost:" + os.Getenv("BACKEND_PORT") + "/api/v1/verify/" + key
+	var link string
+	if strings.Contains(host, "localhost") {
+		link = "http://localhost:" + os.Getenv("PROJECT_PORT") + "/api/v1/verify/" + key
+	} else {
+		link = "https://aim-love.ga/api/main/verify/" + key
+	}
 	body := `<h3>Hello from Matcha!</h3><p>To verify this email address, follow this <a href="` + link + `">link</a></p>`
 	m.sendEmailFromService(to, "Verify email", body)
 }
 
 func (m *EmailManager) SendEmailVerificationKey(to, key string)  {
-	//link := "https://aim-love.ga/verify/" + key
-	link := fmt.Sprintf("http://localhost:%v/email/verify?key=%v", os.Getenv("FRONT_PORT"), key)
+	var link string
+	if strings.Contains(host, "localhost") {
+		link = fmt.Sprintf("http://localhost:%v/email/verify?key=%v", os.Getenv("PROJECT_PORT"), key)
+	} else {
+		link = fmt.Sprintf("https://aim-love.ga/email/verify?key=%v", key)
+	}
 	body := `<h3>Change email</h3><p>To verify this new email address, follow this <a href="` + link + `">link</a></p>`
 	m.sendEmailFromService(to, "Change email", body)
 }
